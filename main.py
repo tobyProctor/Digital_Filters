5193985d7748e42a2075064728b9df63af8cb674
tobyProctor <toby.proctor93@gmail.com>
2022-06-13 11:11:34 +0100

    Switch from pygame to matplotlib

--- main.py
+++ main.py
@@ -10,66 +10,66 @@ SCREEN_RES_Y = 900
 COLOUR_SUN = (255, 255, 0)
 RANGE = 35
 
-time = np.arange(0, RANGE, 0.01)
-print(time)
-amplitude = np.sin(time) + np.sin(10*time)*0.3 + np.sin(100*time)*0.1
-print(amplitude)
-fft = abs(np.fft.fft(amplitude))
+# pygame.init()
+# screen = pygame.display.set_mode([SCREEN_RES_X, SCREEN_RES_Y])
 
-# Plotting time vs amplitude using plot function from pyplot
-#plt.plot(time, fft)
-plt.plot(time, amplitude)
+# def main():
+#     screen.fill(COLOUR_BACKGROUND)
 
-# Settng title for the plot in blue color
-plt.title('Sine Wave', color='b')
+#     while True:
+#         # Exit on close button
+#         for event in pygame.event.get():
+#             if event.type == pygame.QUIT:
+#                 pygame.quit()
+#                 sys.exit()
 
-# Setting x axis label for the plot
-plt.xlabel('Time'+ r'$\rightarrow$')
+#         screen.fill(COLOUR_BACKGROUND)
+#         for x in range(len(time)):
+#             x_y_modifier = ((SCREEN_RES_X-100)/RANGE)/2
+#             x_x_modifier = SCREEN_RES_Y/4+100
+#             y_y_modifier = -0.1
+#             y_x_modifier = 30+(SCREEN_RES_X/2)
+#             pygame.draw.circle(screen, COLOUR_SUN, ((time[x]*x_y_modifier)+y_x_modifier, (fft[x]*y_y_modifier)+x_x_modifier), 1)
 
-# Setting y axis label for the plot
-plt.ylabel('Sin(time) '+ r'$\rightarrow$')
-
-# Showing grid
-plt.grid()
-
-# Highlighting axis at x=0 and y=0
-plt.axhline(y=0, color='k')
-plt.axvline(x=0, color='k')
-
-# Finally displaying the plot
-plt.show()
-
-game_tick = 0
-
-pygame.init()
-screen = pygame.display.set_mode([SCREEN_RES_X, SCREEN_RES_Y])
+#         for x in range(len(time)):
+#             x_y_modifier = ((SCREEN_RES_X-40)/RANGE)/2
+#             x_x_modifier = SCREEN_RES_Y/4
+#             y_y_modifier = SCREEN_RES_Y/8
+#             y_x_modifier = 20
+#             pygame.draw.circle(screen, COLOUR_SUN, ((time[x]*x_y_modifier)+y_x_modifier, (amplitude[x]*y_y_modifier)+x_x_modifier), 2)
+#             #print("{}, {}".format((time[x]*(SCREEN_RES_X/RANGE)), ((amplitude[x]*(SCREEN_RES_Y/4))+(SCREEN_RES_Y/2))))
+#             pygame.display.flip()
 
 def main():
-    screen.fill(COLOUR_BACKGROUND)
+    time = np.arange(0, RANGE, 0.01)
+    amplitude = np.sin(time) + np.sin(10*time)*0.3 + np.sin(100*time)*0.5 + np.sin(130*time)*0.1 
+    fft = abs(np.fft.fft(amplitude))
 
-    while True:
-        # Exit on close button
-        for event in pygame.event.get():
-            if event.type == pygame.QUIT:
-                pygame.quit()
-                sys.exit()
+    fig = plt.figure(figsize=(10, 9))
+    axes = fig.subplots(nrows=1, ncols=2)
 
-        screen.fill(COLOUR_BACKGROUND)
-        for x in range(len(time)):
-            x_y_modifier = ((SCREEN_RES_X-100)/RANGE)/2
-            x_x_modifier = SCREEN_RES_Y/4+100
-            y_y_modifier = -0.1
-            y_x_modifier = 30+(SCREEN_RES_X/2)
-            pygame.draw.circle(screen, COLOUR_SUN, ((time[x]*x_y_modifier)+y_x_modifier, (fft[x]*y_y_modifier)+x_x_modifier), 1)
+    # Plotting time vs amplitude using plot function from pyplot
+    axes[1].plot(time, amplitude)
+    axes[0].plot(fft)
 
-        for x in range(len(time)):
-            x_y_modifier = ((SCREEN_RES_X-40)/RANGE)/2
-            x_x_modifier = SCREEN_RES_Y/4
-            y_y_modifier = SCREEN_RES_Y/8
-            y_x_modifier = 20
-            pygame.draw.circle(screen, COLOUR_SUN, ((time[x]*x_y_modifier)+y_x_modifier, (amplitude[x]*y_y_modifier)+x_x_modifier), 2)
-            #print("{}, {}".format((time[x]*(SCREEN_RES_X/RANGE)), ((amplitude[x]*(SCREEN_RES_Y/4))+(SCREEN_RES_Y/2))))
-            pygame.display.flip()
+    # Settng title for the plot in blue color
+    plt.title('Sine Wave', color='b')
+
+    # Setting x axis label for the plot
+    plt.xlabel('Time'+ r'$\rightarrow$')
+
+    # Setting y axis label for the plot
+    plt.ylabel('Sin(time) '+ r'$\rightarrow$')
+
+    # Showing grid
+    plt.grid()
+
+    # Highlighting axis at x=0 and y=0
+    plt.axhline(y=0, color='k')
+    plt.axvline(x=0, color='k')
+
+    # Finally displaying the plot
+    plt.show()
 
 if __name__ == "__main__":
-    main()
+     main()
